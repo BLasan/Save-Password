c9feeb168c917cdb477cc600310b5c0ff2b37d60
BLasan <benuraab@gmail.com>
2020-05-21 22:57:28 +0530

    changed table name

--- home_view/models.py
+++ home_view/models.py
@@ -6,3 +6,6 @@ class Users(models.Model):
     user_name = models.CharField(max_length=100,default="Unknown",null=False)
     user_email = models.CharField(max_length=250,null=False,primary_key=True)
     password = models.CharField(null=False,max_length=100)
+
+    class Meta:
+        db_table = "reg_users"
