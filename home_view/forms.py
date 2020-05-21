780b61139bd6ca4412527af55b468f7672fcfbdc
BLasan <benuraab@gmail.com>
2020-05-21 22:15:51 +0530

    signup form validations added

--- home_view/forms.py
+++ home_view/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.validators import RegexValidator
+
+password_pattern = RegexValidator(regex=r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}$',message="Must contain at least uppercase character,lowercase character and special character.")
 
 class LoginForm(forms.Form):
     email = forms.EmailField(required=True,help_text='abcd@gmail.com',widget=forms.EmailInput(attrs={'placeholder':'Email'}),label="Email",error_messages={'required':'Please Enter the Email!'})
@@ -6,6 +9,5 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
     email = forms.EmailField(required=True,help_text='abcd@gmail.com',widget=forms.EmailInput(attrs={'placeholder':'Email'}),label="Email",error_messages={'required':'Please Enter the Email!'})
-    password = forms.CharField(required=True,min_length=6,widget=forms.PasswordInput(attrs={'placeholder':'Password'}),label="Password",error_messages={'required':'Please Enter the Password!'})
+    password = forms.CharField(required=True,min_length=6,widget=forms.PasswordInput(attrs={'placeholder':'Password'}),label="Password",validators=[password_pattern],error_messages={'required':'Please Enter the Password!','min_length':'Password should contain at least 6 characters.'})
     rePassword = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'placeholder':'Re Enter Password'}), label="Re Enter Password")
-
--- home_view/views.py
+++ home_view/views.py
@@ -15,7 +15,16 @@ def index(response):
 def signup(response):
     if response.method == "POST":
         signupForm = SignupForm(response.POST)
+        password = signupForm['password'].value()
+        re_password = signupForm['rePassword'].value()
+
+        if(password != re_password):
+            print("Password not matched!")
+            hasError = True
+        else:
+            hasError = False
     else:
         signupForm = SignupForm()
+        hasError = False
     
-    return render(response, "home_view/signup.html",{"form":signupForm})
+    return render(response, "home_view/signup.html",{"form":signupForm,"hasError":hasError})
