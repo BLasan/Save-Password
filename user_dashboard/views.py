7b6f964b1721fa1c09384668c352648496b2d2b0
BLasan <benuraab@gmail.com>
2020-05-26 23:04:59 +0530

    error handelling added for collections

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -22,24 +22,54 @@ def machine_details(response):
     #email = response.session['email']
     email = 'benuraab@gmail.com'
     print(email)
-    machine_data = db[email].find({'type': 'machine_details'}).max_await_time_ms(5000)
-    print(machine_data[0])
-    return render(response, "user_dashboard/machine_details.html", {'machine_data': machine_data[0]})
+    try:
+        if(db[email].find({'type': 'machine_details'}).count()>0):  
+            machine_data = db[email].find({'type': 'machine_details'}).max_await_time_ms(5000)
+            isEmpty = False
+        else:
+            machine_data = list()
+            machine_data.append(list())
+            isEmpty = False
+        return render(response, "user_dashboard/machine_details.html", {'machine_data': machine_data[0], 'isEmpty': isEmpty})
+    except:
+        print("Collection not exists!")
+        return render(response, "user_dashboard/error.html")
 
 def history(response):
     #email = response.session['email']
     email = 'benuraab@gmail.com'
-    history = db[email].find({'type': 'history'}).max_await_time_ms(5000)
-    print(history[0])
-    return render(response, "user_dashboard/history.html", {'history_array': history[0]})
+    try:
+        if(db[email].find({'type':'history'}).count()>0):
+            history = db[email].find({'type': 'history'}).max_await_time_ms(5000)
+            isEmpty = False
+        else:
+            print("Empty")
+            history = list()
+            history.append(list())
+            isEmpty = True
+        return render(response, "user_dashboard/history.html", {'history_array': history[0], 'isEmpty': isEmpty})
+    except:
+        print("Not Found!")
+        return render(response, "user_dashboard/error.html")
 
 def login_data(response):
     email = "benuraab@gmail.com"
-    login_data = db[email].find({'type': 'login_data'}).max_await_time_ms(5000)
-    print(login_data[0])
-    return render(response, "user_dashboard/login_data.html", {'login_data': login_data[0]})
+    try:
+        if(db[email].find({'type': 'login_data'}).count()>0):
+            login_data = db[email].find({'type': 'login_data'}).max_await_time_ms(5000)
+            isEmpty = False
+        else:
+            print("Empty")
+            login_data = list()
+            login_data.append(list())
+            isEmpty = False
+        return render(response, "user_dashboard/login_data.html", {'login_data': login_data[0], 'isEmpty': isEmpty})
+    except:
+        print("Not Found!")
+        return render(response, "user_dashboard/error.html")
 
 def download_zip(response):
     path = "/Download-ZIP/save_password_exe.tar.gz"
     zip_file = open(path, 'rb')
     return FileResponse(zip_file)
+
