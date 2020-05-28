ffc98903fcb3a07da63ee51c7f5184cafbc5ab4d
BLasan <benuraab@gmail.com>
2020-05-28 22:22:44 +0530

    converted bookmark date to standard

---
+++ user_dashboard/templatetags/custom_date_tag.py
@@ -0,0 +1,12 @@
+from django import template  
+import datetime  
+register = template.Library()    
+
+@register.filter('timestamp_to_time')
+def timestamp_to_time(timestamp):
+    seconds, micros = divmod(int(timestamp), 1000000)
+    days, seconds = divmod(seconds, 86400)
+    date = datetime.datetime(1601, 1, 1) + datetime.timedelta(days, seconds, micros)
+    return date.strftime('%a, %d %B %Y %H:%M:%S %Z')
+    # print(date.strftime('%a, %d %B %Y %H:%M:%S %Z'))
+  
